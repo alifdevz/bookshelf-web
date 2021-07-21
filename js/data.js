c9826046c0691dcb53c65513bd4af065a06e1543
alifdevz <alifm.devz@gmail.com>
2021-07-21 20:53:32 +0800

    Added isStorageExist() function

--- js/data.js
+++ js/data.js
@@ -1,2 +1,10 @@
 const STORAGE_KEY = 'BOOKSHELF_APP';
 let books = [];
+
+function isStorageExist() {
+    if (typeof(Storage) === undefined) {
+        alert('Browser Anda tidak mendukung local storage.');
+        return false;
+    }
+    return true;
+}
